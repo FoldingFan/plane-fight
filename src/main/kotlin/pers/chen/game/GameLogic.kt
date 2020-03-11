a2e334a985b7b64b24f0d684a19fab81742386d5
liuchen <361130953@qq.com>
2020-03-11 22:01:28 +0800

    游戏逻辑 对象建立

--- src/main/kotlin/pers/chen/game/GameLogic.kt
+++ src/main/kotlin/pers/chen/game/GameLogic.kt
@@ -10,18 +10,18 @@ import pers.chen.game.entity.HeroFly
  */
 object GameLogic {
     fun left() {
-        HeroFly.centerX = HeroFly.centerX - 1
+        HeroFly.centerX = HeroFly.centerX - 4
     }
 
     fun up() {
-        HeroFly.centerY = HeroFly.centerY - 1
+        HeroFly.centerY = HeroFly.centerY - 4
     }
 
     fun right() {
-        HeroFly.centerX = HeroFly.centerX + 1
+        HeroFly.centerX = HeroFly.centerX + 4
     }
 
     fun down() {
-        HeroFly.centerY = HeroFly.centerY + 1
+        HeroFly.centerY = HeroFly.centerY + 4
     }
 }
