98b06e3a176065efd5bad1b8b3d06e8170d4e592
liuchen <361130953@qq.com>
2020-03-11 16:05:49 +0800

    初始化

--- src/main/java/pers/chen/App.kt
+++ src/main/java/pers/chen/App.kt
@@ -19,27 +19,7 @@ import java.io.File
  *
  */
 class App : Application() {
-    private val textField = TextField()
-    private val button = Button("输出")
-    private val label = Label()
     override fun start(stage: Stage) {
-        label.textAlignment = TextAlignment.CENTER
-        val vBox = VBox(20.0, textField, button, label)
-        vBox.alignment = Pos.CENTER
-        button.setOnAction {
-            val file = File("./a.txt")
-            if (!file.exists()) {
-                file.createNewFile()
-            }
-            if (file.canWrite()) {
-                file.writeText(textField.text)
-                label.text = "输出成功，输出目录:${file.absolutePath}"
-            } else {
-                label.text = "文件不可用"
-            }
-        }
-        val root = StackPane(vBox)
-        stage.scene = Scene(root, 500.0, 500.0)
         stage.show()
     }
 }
