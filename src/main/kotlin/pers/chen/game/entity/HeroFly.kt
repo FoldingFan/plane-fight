965d9aac6d96c3bb9b340ebde6db2e315de2a0ad
liuchen <361130953@qq.com>
2020-03-11 23:29:30 +0800

    移动方法修复

--- src/main/kotlin/pers/chen/game/entity/HeroFly.kt
+++ src/main/kotlin/pers/chen/game/entity/HeroFly.kt
@@ -46,7 +46,7 @@ object HeroFly : GameObj() {
             speed /= 5
             speed = if (speed <= 10) 0.0 else speed
         } else if (speed < 120) {
-            speed += (120 - speed) / 2
+            speed += (120 - speed) / 5
             speed = if (speed >= 110) 120.0 else speed
         }
     }
