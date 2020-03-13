43dacecb93d75e6bd1736320a53b8b106ba73092
liuchen <361130953@qq.com>
2020-03-13 11:20:03 +0800

    开火功能

--- src/main/kotlin/pers/chen/framwork/MainCoroutines.kt
+++ src/main/kotlin/pers/chen/framwork/MainCoroutines.kt
@@ -2,6 +2,7 @@ package pers.chen.framwork
 
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 /**
  * @Author: chen
@@ -10,29 +11,50 @@ import kotlinx.coroutines.launch
  *  主协程
  */
 object MainCoroutines {
-    private var elapsedNano: Long = 0
-    private var preNano: Long? = null
+
+
+    /*设置的fps*/
     var fps: Long = 0
         set(value) {
             field = value
             nps = (1E9 / field).toLong()
         }
-    var nps: Long = 0
+
+    /*实际的Fps*/
+    var realFps = 0
+
+
+    private var elapsedNano: Long = 0
+    private var preNano: Long? = null
+    private var nps: Long = 0
+
+    /*统计真实fps*/
+    private val fpsArray = arrayListOf<Long>()
 
     init {
         fps = 60
     }
 
+    private fun realFpsCalc(elapsedNano: Long) {
+        fpsArray.add(elapsedNano)
+        if (fpsArray.size == fps.toInt()) {
+            val nanoFrame = fpsArray.average()
+            realFps = (1E9 / nanoFrame).roundToInt()
+            fpsArray.clear()
+        }
+    }
+
     /*游戏线程开始*/
     fun play() {
         GlobalScope.launch {
+            println(Thread.currentThread().name)
             while (true) {
                 val currNano = System.nanoTime()
-                preNano = preNano
-                        ?: currNano
+                preNano = preNano ?: currNano
                 elapsedNano += currNano - preNano!!
                 if (elapsedNano >= nps) {
                     ViewManager.currView?.onUpdate(elapsedNano)
+                    realFpsCalc(elapsedNano)
                     elapsedNano -= nps
                 }
                 preNano = currNano
--- src/main/kotlin/pers/chen/game/GameManager.kt
+++ src/main/kotlin/pers/chen/game/GameManager.kt
@@ -5,9 +5,9 @@ import javafx.scene.canvas.GraphicsContext
 import javafx.scene.paint.Color
 import pers.chen.framwork.GAME_HEIGHT
 import pers.chen.framwork.GAME_WIDHT
+import pers.chen.framwork.MainCoroutines
 import pers.chen.game.beans.AbstractGameObject
 import pers.chen.game.beans.HeroFly
-import java.util.concurrent.CopyOnWriteArraySet
 
 /**
  * @Author: chen
@@ -16,8 +16,15 @@ import java.util.concurrent.CopyOnWriteArraySet
  * 游戏管理
  */
 object GameManager {
-    /*线程安全的set*/
-    private val gameObjList = CopyOnWriteArraySet<AbstractGameObject>()
+    /*游戏对象集合*/
+    private val gameObjList = hashSetOf<AbstractGameObject>()
+
+    /*待删除游戏对象*/
+    private val gameObjListRemove = hashSetOf<AbstractGameObject>()
+
+    /*待添加游戏对象*/
+    private val gameObjListAdd = hashSetOf<AbstractGameObject>()
+
 
     /*游戏画板*/
     val canvas = Canvas(GAME_WIDHT, GAME_HEIGHT)
@@ -29,24 +36,28 @@ object GameManager {
 
 
     fun addObj(obj: AbstractGameObject) {
-        gameObjList.add(obj)
+        gameObjListAdd.add(obj)
     }
 
     fun removeObj(obj: AbstractGameObject) {
-        gameObjList.remove(obj)
+        gameObjListRemove.remove(obj)
     }
 
     /**
      * 每帧调用
      */
     fun run(nano: Long) {
-        graphics.fill = Color.web("#000000")
+        graphics.fill = Color.BLACK
         graphics.fillRect(0.0, 0.0, GAME_WIDHT, GAME_HEIGHT)
+        graphics.fill = Color.WHITE
+        graphics.fillText("FPS:${MainCoroutines.realFps}", 10.0, 20.0)
         gameObjList.forEach {
-            it.move()
             it.update(nano)
+            it.move()
             it.draw(graphics)
         }
+        gameObjList.addAll(gameObjListAdd)
+        gameObjList.removeAll(gameObjListRemove)
     }
 
 
